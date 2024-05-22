3744780d89408ec20a9ba651318e272560037dd3
Joy <joy.chiaying@gmail.com>
2024-05-22 16:57:00 +0800

    setup properties and test gecko api

---
+++ DataServer/src/main/java/com/joycodes/dataserver/controller/PingController.java
@@ -0,0 +1,39 @@
+package com.joycodes.dataserver.controller;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ping")
+@PropertySource("classpath:coingecko.properties")
+@CrossOrigin
+public class PingController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("test-connection")
+    public String ping() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(env.getProperty("gecko.ping.url"))
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("x-cg-demo-api-key", env.getProperty("gecko.api-key"))
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
